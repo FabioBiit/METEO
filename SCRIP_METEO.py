dd8f06b97bc7de494cecdea608bb0a8159b336e6
FabioBiit <asusdioasus@gmail.com>
2025-01-12 19:20:00 +0100

    Aggiornamenti e aggiunta di nuove città

--- SCRIP_METEO.py
+++ SCRIP_METEO.py
@@ -1,25 +1,18 @@
 import requests
 import pandas as pd
-# import com.microsoft.spark.sqlanalytics
 from datetime import datetime
 
 data_time_stamp = datetime.now().replace(microsecond=0)
 
-# print(data_time_stamp)
-
-# milano = "https://api.openweathermap.org/data/2.5/weather?q=Milano&appid=d772ae99953d3504ba931841f8bd77da&units=metric"
-# bologna = "https://api.openweathermap.org/data/2.5/weather?q=Bologna&appid=d772ae99953d3504ba931841f8bd77da&units=metric"
-# cagliari = "https://api.openweathermap.org/data/2.5/weather?q=Cagliari&appid=d772ae99953d3504ba931841f8bd77da&units=metric"
-
-citta = ["milano", "bologna", "cagliari"]
+citta = ["milano", "bologna", "cagliari", "palermo", "napoli"]
 
 dataframe = {} # Diz per lo storage dei 3 dataframe
 
 for i, city in enumerate(citta):
 
-    citta2 = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d772ae99953d3504ba931841f8bd77da&units=metric"
+    citta_str = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d772ae99953d3504ba931841f8bd77da&units=metric"
 
-    response = requests.get(citta2)
+    response = requests.get(citta_str)
 
     # Verificare che la richiesta sia andata a buon fine
     if response.status_code == 200:
@@ -38,7 +31,6 @@ for i, city in enumerate(citta):
         }
     
         df_pd = pd.DataFrame([dati])
-
         dataframe[f"df_{i+1}"] = df_pd
 
     else:
@@ -46,15 +38,14 @@ for i, city in enumerate(citta):
 
 df_final = pd.concat(dataframe, ignore_index=True).drop_duplicates() # Unione dei 3 dataframe creati in precedenza in un unico DF
 
-df_final['City'] = df_final['City'].replace('Provincia di Cagliari', 'Cagliari') # Rinomino un valore per una migliore lettura
+df_final['City'] =  df_final['City'].replace('Provincia di Cagliari', 'Cagliari')\
+                                    .replace('Province of Palermo', 'Palermo') # Rinomino valori per una migliore lettura
 
 print(df_final)
 
 # Metodo per salvare in append su una tabella nel DB SQL di AZURE SYNAPSE
 # df_final.write.mode("append").synapsesql("nome_tab_SQL")
 
-# df_final.to_csv("C:/Users/kyros/OneDrive/Desktop/dataframe_meteo_2.csv", mode='a', index=False, header=False) # Salvo il DF come CSV per simulare il save su DB
+df_final.to_csv("C:/Users/kyros/OneDrive/Desktop/dataframe_meteo_2.csv", mode='a', index=False, header=False) # Salvo il DF come CSV per simulare il save su DB
 
 # df_final.info()
-
-
