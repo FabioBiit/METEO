181835aad082daf1b66df5c3abdb9f8d4efd6d66
FabioBiit <asusdioasus@gmail.com>
2025-01-15 07:51:06 +0100

    Aggiornamenti automatici

--- GIT_COMMAND.py
+++ GIT_COMMAND.py
@@ -1,22 +1,13 @@
-import os
 import subprocess
 
-# Cambia directory al repository
-os.chdir("C:/Users/kyros/OneDrive/Desktop/METEO")
-
-# Funzione per eseguire i seguenti comandi git:
-def git_command(cmd):
-    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+def run_git_command(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode == 0:
-        print(f"Succeeded: {result.stdout}")
+        print(f"Successo: {result.stdout}")
     else:
-        print(f"Failed: {result.stderr}")
+        print(f"Errore: {result.stderr}")
 
-try:
-    git_command("git pull")
-    git_command("git add .")
-    git_command("git commit -m 'Aggiornamento repository'")
-    git_command("git push")
-
-except Exception as e:
-    print(f"Errore durante l'esecuzione: {e}")
+run_git_command("git pull")
+run_git_command("git add .")
+run_git_command('git commit -m "Aggiornamenti automatici"')
+run_git_command("git push")
